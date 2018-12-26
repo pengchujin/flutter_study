ab0ec5327bd2a205f9589102f9526fffe34b64e3
pengchujin <pengchujin@blackbeanssoft.com>
2018-12-27 00:45:29 +0800

    flutter studty start

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Fluter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+    
+  }
+}
+
+class NewRoute extends StatelessWidget {
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('test new route'),
+      ),
+      body: Center(
+        child: Text('this is new route'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  
+  void _incrementCounter() {
+    setState(() {
+          _counter++;
+        });
+  }
+  @override 
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar:  new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'You have pushed the button this many times:'
+            ),
+            new Text('$_counter',
+            style: Theme.of(context).textTheme.display1),
+            FlatButton(
+              child: Text('open new route'),
+              textColor: Colors.blue,
+              onPressed: () {
+                Navigator.push(context, new MaterialPageRoute(builder: (context)
+                {
+                  return new NewRoute();
+                }));
+              },
+              )
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+      onPressed: _incrementCounter,
+      tooltip: 'Increment',
+      child: new Icon(Icons.add),
+      ),
+    );
+
+  }
+}
